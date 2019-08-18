f04e91f345ccf550a7591fff53842599aad9cfa3
Christina Gonzalez <cmgamway@gmail.com>
2019-08-18 07:52:10 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -7,6 +7,6 @@ end
 def assign_rooms
   room_assignments = [1, 2, 3, 4, 5, 6, 7]
   room_assignments.each do |room_assignments|
-    puts "Hello, #{attendee}! You'll be assigned to #{room_assignments}"
+    puts "Hello, #{attendee}! You'll be assigned to #{room_assignments}!"
   end
 end
